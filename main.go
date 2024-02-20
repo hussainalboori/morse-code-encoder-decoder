d357982731eded27301a67efb3716b432f982c98
hussain alboori <34797449+hussainalboori@users.noreply.github.com>
2024-02-20 13:35:31 +0300

    adding a func that detect morse code

--- main.go
+++ main.go
@@ -27,6 +27,20 @@ var morseDict = map[string]string{
 	"0": "-----", " ": "/",
 }
 
+func isMorseCode(input string) bool {
+	// Define a set of Morse code characters
+	morseChars := []string{".", "-", " ", "/"}
+
+	// Check if the input string only contains Morse code characters
+	for _, char := range input {
+		if !strings.ContainsAny(string(char), strings.Join(morseChars, "")) {
+			return false
+		}
+	}
+
+	return true
+}
+
 func main(){
 	var morseCodeInpout string = "gg gg"
 	// fmt.Println("enter the massge you want to encode or decode:")
