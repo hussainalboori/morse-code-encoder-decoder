fc07b5e6f9ead3fb13dd44f169cb7fcac9533108
hussain alboori <34797449+hussainalboori@users.noreply.github.com>
2024-02-20 10:21:48 +0300

    fixing decoding function

--- main.go
+++ main.go
@@ -16,11 +16,22 @@ var morseCode = map[string]string{
 	"-----": "0", "/": " ",
 }
 
+var morseDict = map[string]string{
+	"A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".",
+	"F": "..-.", "G": "--.", "H": "....", "I": "..", "J": ".---",
+	"K": "-.-", "L": ".-..", "M": "--", "N": "-.", "O": "---",
+	"P": ".--.", "Q": "--.-", "R": ".-.", "S": "...", "T": "-",
+	"U": "..-", "V": "...-", "W": ".--", "X": "-..-", "Y": "-.--",
+	"Z": "--..", "1": ".----", "2": "..---", "3": "...--", "4": "....-",
+	"5": ".....", "6": "-....", "7": "--...", "8": "---..", "9": "----.",
+	"0": "-----", " ": "/",
+}
+
 func main(){
-	var morseCodeInpout string = "--. --. / --. --."
+	var morseCodeInpout string = "gg gg"
 	// fmt.Println("enter the massge you want to encode or decode:")
 	// fmt.Scan(&morseCodeInpout)
-	messge := decodeMorseCode(morseCodeInpout)
+	messge := encodeMorseCode(morseCodeInpout)
 	fmt.Println(messge)
 	//fmt.Println(morseCodeInpout)
 }
@@ -41,4 +52,18 @@ func decodeMorseCode(code string) string {
 	}
 
 	return decodedMessage
+}
+
+func encodeMorseCode(text string) string{
+	text = strings.ToUpper(text)
+	encodedMessage := ""
+
+	for _, char := range text {
+		encodedSymbol, exists := morseDict[string(char)]
+		if exists {
+			encodedMessage += encodedSymbol + " "
+		}
+	}
+
+	return encodedMessage
 }
