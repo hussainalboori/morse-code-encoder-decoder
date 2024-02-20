349fd2238ecaea229a191a78463841cc32203899
hussain alboori <34797449+hussainalboori@users.noreply.github.com>
2024-02-20 10:00:06 +0300

    adding decoing func

--- main.go
+++ main.go
@@ -1,50 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 var morseCode = map[string]string{
-	"A": ".-",
-	"B": "-...",
-	"C": "-.-.",
-	"D": "-..",
-	"E": ".",
-	"F": "..-.",
-	"G": "--.",
-	"H": "....",
-	"I": "..",
-	"J": ".---",
-	"K": "-.-",
-	"L": ".-..",
-	"M": "--",
-	"N": "-.",
-	"O": "---",
-	"P": ".--.",
-	"Q": "--.-",
-	"R": ".-.",
-	"S": "...",
-	"T": "-",
-	"U": "..-",
-	"V": "...-",
-	"W": ".--",
-	"X": "-..-",
-	"Y": "-.--",
-	"Z": "--..",
-	"1": ".----",
-	"2": "..---",
-	"3": "...--",
-	"4": "....-",
-	"5": ".....",
-	"6": "-....",
-	"7": "--...",
-	"8": "---..",
-	"9": "----.",
-	"0": "-----",
+	".-":   "A", "-...": "B", "-.-.": "C", "-..":  "D", ".":    "E",
+	"..-.": "F", "--.":  "G", "....": "H", "..":   "I", ".---": "J",
+	"-.-":  "K", ".-..": "L", "--":   "M", "-.":   "N", "---":  "O",
+	".--.": "P", "--.-": "Q", ".-.":  "R", "...":  "S", "-":    "T",
+	"..-":  "U", "...-": "V", ".--":  "W", "-..-": "X", "-.--": "Y",
+	"--..": "Z", ".----": "1", "..---": "2", "...--": "3", "....-": "4",
+	".....": "5", "-....": "6", "--...": "7", "---..": "8", "----.": "9",
+	"-----": "0", "/": " ",
 }
 
 func main(){
-	var morseCodeInpout string
-	fmt.Println("enter the massge you want to encode or decode:")
-	fmt.Scan(&morseCodeInpout)
-	
+	var morseCodeInpout string = "--. --. / --. --."
+	// fmt.Println("enter the massge you want to encode or decode:")
+	// fmt.Scan(&morseCodeInpout)
+	messge := decodeMorseCode(morseCodeInpout)
+	fmt.Println(messge)
+	//fmt.Println(morseCodeInpout)
 }
 
+func decodeMorseCode(code string) string {
+	words := strings.Split(code, " / ")
+	decodedMessage := ""
+
+	for _, word := range words {
+		symbols := strings.Split(word, " ")
+		for _, symbol := range symbols {
+			decodedSymbol, exists := morseCode[symbol]
+			if exists {
+				decodedMessage += decodedSymbol
+			}
+		}
+		decodedMessage += " "
+	}
+
+	return decodedMessage
+}
